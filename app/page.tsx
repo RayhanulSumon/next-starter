d70dc6543e3479bcf8a910d19e6c7187b0b8950a
RayhanulSumon <rayhanulsumon@gmail.com>
2025-08-23 21:33:26 +0600

    home  page route fix

--- app/page.tsx
+++ app/page.tsx
@@ -23,7 +23,7 @@ const Page = () => {
         </CardHeader>
         <CardContent className="flex justify-center">
           <a
-            href="/app/(auth)/login"
+            href="/login"
             className="inline-block px-6 py-3 bg-gradient-to-r from-blue-600 to-purple-600 text-white font-semibold rounded-lg shadow hover:scale-105 hover:shadow-xl transition-transform duration-200"
           >
             Get Started
