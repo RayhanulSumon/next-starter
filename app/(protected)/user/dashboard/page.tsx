fb0ea9c38dd9389b50399f670a95199bddc63c57
RayhanulSumon <rayhanulsumon@gmail.com>
2025-08-29 00:29:16 +0600

    update dashboard page use force dynamic

--- app/(protected)/user/dashboard/page.tsx
+++ app/(protected)/user/dashboard/page.tsx
@@ -1,4 +1,6 @@
 "use client";
+export const dynamic = "force-dynamic";
+
 import { useAuth } from "@/hook/useAuth";
 import { UserRole } from "@/types/auth";
 import { useState, useEffect } from "react";
