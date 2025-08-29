7ad01570aedcf3bf9fed23478e20ddf4b0c316e9
RayhanulSumon <rayhanulsumon@gmail.com>
2025-08-29 13:46:40 +0600

    jest test type fixed

--- jest.setup.ts
+++ jest.setup.ts
@@ -3,42 +3,64 @@
 
 import "@testing-library/jest-dom";
 import { TextEncoder, TextDecoder } from 'util';
-import fetch, { Request, Response, Headers } from 'node-fetch';
+import fetch, { Request, Response, Headers } from 'cross-fetch';
 
 if (typeof global.TextEncoder === 'undefined') {
-  global.TextEncoder = TextEncoder;
+  global.TextEncoder = TextEncoder as unknown as typeof global.TextEncoder;
 }
 if (typeof global.TextDecoder === 'undefined') {
-  global.TextDecoder = TextDecoder;
+  global.TextDecoder = TextDecoder as unknown as typeof global.TextDecoder;
 }
 
 if (typeof global.fetch === 'undefined') {
-  global.fetch = fetch as any;
+  global.fetch = fetch;
 }
 if (typeof global.Request === 'undefined') {
-  global.Request = Request as any;
+  global.Request = Request;
 }
 if (typeof global.Response === 'undefined') {
-  global.Response = Response as any;
+  global.Response = Response;
 }
 if (typeof global.Headers === 'undefined') {
-  global.Headers = Headers as any;
+  global.Headers = Headers;
 }
 
 // Mock window.matchMedia for jsdom environment
 if (typeof window !== "undefined" && !window.matchMedia) {
-  window.matchMedia = function (query) {
+  window.matchMedia = function (query: string): MediaQueryList {
+    let listeners: EventListenerOrEventListenerObject[] = [];
     return {
       matches: false,
       media: query,
       onchange: null,
-      addListener: function () {},
-      removeListener: function () {},
-      addEventListener: function () {},
-      removeEventListener: function () {},
+      addEventListener: function (
+        _type: string,
+        listener: EventListenerOrEventListenerObject,
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        _options?: boolean | AddEventListenerOptions
+      ) {
+        listeners.push(listener);
+      },
+      removeEventListener: function (
+        _type: string,
+        listener: EventListenerOrEventListenerObject,
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        _options?: boolean | EventListenerOptions
+      ) {
+        listeners = listeners.filter(l => l !== listener);
+      },
       dispatchEvent: function () {
         return false;
       },
-    };
+      // Deprecated methods for backward compatibility
+      addListener(listener: EventListenerOrEventListenerObject) {
+        // Deprecated: use addEventListener instead
+        this.addEventListener('change', listener);
+      },
+      removeListener(listener: EventListenerOrEventListenerObject) {
+        // Deprecated: use removeEventListener instead
+        this.removeEventListener('change', listener);
+      },
+    } as MediaQueryList;
   };
 }
